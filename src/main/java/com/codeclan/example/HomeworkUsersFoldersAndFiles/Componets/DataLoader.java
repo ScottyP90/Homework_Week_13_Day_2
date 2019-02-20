b7acd279b14e7db0f175ec676a4d4f4be82f89dd
Iain Scott Prentice <iainscottyp@hotmail.com>
2019-02-20 06:13:31 +0000

    small change

--- src/main/java/com/codeclan/example/HomeworkUsersFoldersAndFiles/Componets/DataLoader.java
+++ src/main/java/com/codeclan/example/HomeworkUsersFoldersAndFiles/Componets/DataLoader.java
@@ -9,7 +9,10 @@ import com.codeclan.example.HomeworkUsersFoldersAndFiles.Repository.UserReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
